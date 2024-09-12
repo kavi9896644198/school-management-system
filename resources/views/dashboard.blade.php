29607567be7ed5c302b033c4ebadcd0dd038fa5c
webutopian <webutopian@gmmail.com>
2024-09-12 14:56:33 +0530

    school management system setup on laravel project and header footer sidebar all set

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/dashboard.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+	<h1>Welcome to the Dashboard</h1>
+	<p>This is the main content area.</p>
+@endsection
